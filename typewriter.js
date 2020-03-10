6f944a907460bfd4acead42aad22e85476f14a88
headhuntar@gmail.com <headhuntar@gmail.com>
2020-03-10 20:33:32 +0000

    added end line

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,13 @@
 const sentence = "hello there from lighthouse labs";
 let count = 0;
 for (const char of sentence) {
+  if (count === sentence.length - 1) {
+    setTimeout(() => {
+      console.log('');
+    }, (count + 1) * 50);
+  }
   setTimeout(() => {
     process.stdout.write(char);
-  }, count * 50) // <= 1s delay to make it noticeable. Can dial it down later.
+  }, count * 50); // <= 1s delay to make it noticeable. Can dial it down later.
   count++;
-}
+};
